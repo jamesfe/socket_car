e22863698d31ca02cb2f5aa7c0224eccecbea867
jamesfe <james.ferrara@gmail.com>
2017-08-07 22:02:55 +0200

    Linting

--- server_car/car_serve/car_serve.py
+++ server_car/car_serve/car_serve.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
+"""The main entry point for our little car. """
 
 import tornado.ioloop
 import tornado.web
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
+
 
 if __name__ == "__main__":
     app = make_app()
