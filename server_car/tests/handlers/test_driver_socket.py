37dd6bf5539ac4dcfe6f15b8d8d5ac3121865dea
jamesfe <james.ferrara@gmail.com>
2017-08-15 20:48:22 +0200

    tests

--- server_car/tests/handlers/test_driver_socket.py
+++ server_car/tests/handlers/test_driver_socket.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 from tornado.testing import gen_test, AsyncHTTPTestCase
 from tornado.websocket import websocket_connect
+
 from car_serve.car_serve import CarServer
 
 
@@ -15,10 +18,70 @@ class TestDriverSocket(AsyncHTTPTestCase):
         return self.test_app
 
     @gen_test
-    def test_messages_are_received_and_replied_to(self):
+    def test_non_json_messages_return_error(self):
         ws = yield websocket_connect(
             self.test_url % self.get_http_port(),
             io_loop=self.io_loop)
         ws.write_message('blaggle')
         response = yield ws.read_message()
         self.assertIsNotNone(response)
+        res = json.loads(response)
+        self.assertIsInstance(res, dict)
+        msg = {'type': 'error', 'message': 'not json'}
+        self.assertDictEqual(res, msg)
+
+    @gen_test
+    def test_messages_are_received_and_replied_to(self):
+        ws = yield websocket_connect(
+            self.test_url % self.get_http_port(),
+            io_loop=self.io_loop)
+        ws.write_message(json.dumps({'blaggle': True}))
+        response = yield ws.read_message()
+        self.assertIsNotNone(response)
+
+    @gen_test
+    def test_messages_with_no_purpose_receive_error(self):
+        ws = yield websocket_connect(
+            self.test_url % self.get_http_port(),
+            io_loop=self.io_loop)
+        ws.write_message(json.dumps({'blaggle': 'blah'}))
+        response = yield ws.read_message()
+        self.assertIsNotNone(response)
+        res = json.loads(response)
+        self.assertIsInstance(res, dict)
+        msg = {'type': 'error', 'message': 'no purpose'}
+        self.assertDictEqual(res, msg)
+
+    @gen_test
+    def test_turning_turns_the_vehicle(self):
+        turn_message = {
+            'purpose': 'turn',
+            'value': 5,
+            'direction': 'left'
+        }
+        ws = yield websocket_connect(
+            self.test_url % self.get_http_port(),
+            io_loop=self.io_loop)
+        ws.write_message(json.dumps(turn_message))
+        response = yield ws.read_message()
+        self.assertIsNotNone(response)
+        res = json.loads(response)
+        self.assertIsInstance(res, dict)
+        car_state = self.test_app.car_state
+        self.assertEqual(car_state.steering_servo, -5)
+
+    @gen_test
+    def test_zero_all_stops_everything(self):
+        pass
+
+    @gen_test
+    def test_inc_speed_increases_speed(self):
+        pass
+
+    @gen_test
+    def test_dec_speed_decreases_speed(self):
+        pass
+
+    @gen_test
+    def test_stop_does_stop_car(self):
+        pass
