083fd13684c16ffba72d8d9618a78308e69a5310
jamesfe <james.ferrara@gmail.com>
2017-08-17 09:33:37 +0200

    Refactor car state out

---
+++ server_car/car_serve/car_state.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+
+class CarState(object):
+    """This manages the car state."""
+
+    def print_state(self):
+        """Mostly a debug function."""
+        print('{} {} {}'.format(self.steering_servo, self.left_motor, self.right_motor))
+
+    def __init__(self):
+        self.steering_servo = 0
+        self.left_motor = 0
+        self.right_motor = 0
+
+    def _inc_motor(self, begin, val):
+        """A helper function we can change later to modify how values are calculated."""
+        return begin + val
+
+    def _zero_left(self):
+        """Zero out the left motor."""
+        self.left_motor = 0
+
+    def _zero_right(self):
+        """Zero out the right motor."""
+        self.right_motor = 0
+
+    def _zero_steering(self):
+        self.steering_servo = 0
+
+    def zero_steering(self):
+        self._zero_steering()
+
+    def inc_motor(self, choice, val):
+        if choice == 'left':
+            self.left_motor = self._inc_motor(self.left_motor, val)
+        elif choice == 'right':
+            self.right_motor = self._inc_motor(self.right_motor, val)
+        elif choice == 'both':
+            self.left_motor = self._inc_motor(self.left_motor, val)
+            self.right_motor = self._inc_motor(self.right_motor, val)
+
+    def _turn(self, value):
+        """Seemingly stupid, maybe we will have to do something more complex here later."""
+        self.steering_servo += value
+
+    def turn(self, value):
+        """Turn by value: negative is left, positive is right."""
+        self._turn(value)
+
+    def zero_both_motors(self):
+        self._zero_left()
+        self._zero_right()
+
+    def zero_all(self):
+        self.zero_both_motors()
+        self.zero_steering()
+
+    def update_physical_state(self):
+        """Send the right values to the GPIO pins."""
+        pass
