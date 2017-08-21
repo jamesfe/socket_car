804dfa6fc2ff39864fdf3158d54b4cdb355dd561
jamesfe <james.ferrara@gmail.com>
2017-08-21 21:39:52 +0200

    Minor type check

--- server_car/tests/test_car_state.py
+++ server_car/tests/test_car_state.py
@@ -14,3 +14,7 @@ class TestCarState(unittest.TestCase):
         for i in range(10):
             a.update_physical_state()
         self.assertEqual(len(a.previous_states), 5)
+
+    def test_health_check_returns_dict(self):
+        a = car_state.CarState()
+        self.assertIsInstance(a.health_check(), dict)
