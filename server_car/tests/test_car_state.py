044ba93db2b480a28c3b3bd27c53aaa0f8a7ca85
jamesfe <james.ferrara@gmail.com>
2017-08-21 21:33:04 +0200

    Todos for test

--- server_car/tests/test_car_state.py
+++ server_car/tests/test_car_state.py
@@ -2,8 +2,13 @@
 
 import unittest
 
+from socket_car import car_state
+
 
 class TestCarState(unittest.TestCase):
 
     def test_max_intervals(self):
-        pass
+        a = car_state.CarState()
+        # Set max items
+        # Run a few updates
+        # Check length of telemetry
