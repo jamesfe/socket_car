4b4be79cae7524673c7741edc1495368dd9395fd
jamesfe <james.ferrara@gmail.com>
2017-08-22 09:13:42 +0200

    Minor changes

--- server_car/car_serve/handlers/main_handler.py
+++ server_car/car_serve/handlers/main_handler.py
@@ -2,5 +2,7 @@ from tornado.web import RequestHandler
 
 
 class MainHandler(RequestHandler):
+    """I'll just leave this here for posterity."""
+
     def get(self):
-        self.write("Hello, world")
+        self.write('Hello, world')
