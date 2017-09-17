f211e87fe13e1dd3df1af3b9898981384ccf60a3
jamesfe <james.ferrara@gmail.com>
2017-09-17 22:28:11 +0200

    Control stick

--- clientDriver/src/app/app.module.ts
+++ clientDriver/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ControlStickComponent } from './control-stick/control-stick.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlStickComponent
   ],
   imports: [
     BrowserModule,
---
+++ clientDriver/src/app/control-stick/control-stick.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-stick',
+  templateUrl: './control-stick.component.html',
+  styleUrls: ['./control-stick.component.css']
+})
+export class ControlStickComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
