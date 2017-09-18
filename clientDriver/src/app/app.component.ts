4c53ef5f9d077f980d88ecc44193ec58dee0205c
jamesfe <james.ferrara@gmail.com>
2017-09-18 21:50:24 +0200

    Junk templates, add to ts file

--- clientDriver/src/app/app.component.ts
+++ clientDriver/src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "socketcar",
-    templateUrl: './app.component.html',
-    styleUrls: [ "./app.component.scss" ]
+    styleUrls: [ "./app.component.scss" ],
+    template: `
+        <div>
+        <h1>Socket Car Controller</h1>
+        <controlstick></controlstick>
+        </div>
+    `
 })
 
 export class AppComponent implements OnInit {
     
-    public constructor() {}
+    public constructor() {
+        console.log("In app component ts");
+    }
     
     public ngOnInit(): void {}
     
--- clientDriver/src/app/control-stick/control-stick.component.ts
+++ clientDriver/src/app/control-stick/control-stick.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-control-stick',
-  templateUrl: './control-stick.component.html',
-  styleUrls: ['./control-stick.component.css']
+  selector: 'controlstick',
+  template: `
+    <div>
+    <span><button name="zero">Zero</button></span>
+    <span><button>Stop</button></span>
+    <span><button>Inc Speed</button><button>Dec Speed</button></span>
+    <span><button>Wheels Left</button><button>Wheels Right</button></span>
+    </div>
+  `
 })
-export class ControlStickComponent implements OnInit {
+
+export class ControlStickComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
