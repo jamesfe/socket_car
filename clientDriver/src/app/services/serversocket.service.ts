16eb7d3802afea53928f34dda3679f00dc0025bc
jamesfe <james.ferrara@gmail.com>
2017-09-25 21:05:51 +0200

    Hack together something via example

--- clientDriver/src/app/services/serversocket.service.ts
+++ clientDriver/src/app/services/serversocket.service.ts
@@ -1,87 +1,62 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
-import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
 import 'rxjs/add/observable/dom/webSocket';
 
 const DRIVER_URL = 'ws://localhost:9001/control_socket';
 
 export interface Message {
-    toString(): string;
+	purpose: string,
+	message: string,
 }
 
-export class GenMessage implements Message {
-    body: string;
-
-    toString(): string {
-        return "blah";
-    }
-}
-
-interface Frame {
-    type: string;
-    data: any;
-};
-
-@Injectable()
+Injectable()
 export class SocketService {
-    public messages: Observable<Message>;
-    private ws: Subject<any>;
+    // Cheers! From: https://github.com/PeterKassenaar/ng2-websockets/
+    public messages: Subject<Message>  = new Subject<Message>();
 
     constructor() {
-        console.log("Constructing web socket");
-        this.ws = Observable.webSocket(DRIVER_URL);
-        this.messages = makeHot(this.ws).map(parseFrame).filter(m => m != null);
-        console.log("WS Constructed. ", this.ws);
+        this.messages = <Subject<Message>>this.connect()
+            .map((response: MessageEvent): Message => {
+                let data = JSON.parse(response.data);
+                return {
+                    purpose : data.purpose,
+                    message: data.message
+                }
+            });
     }
 
-    sendGenMessage(msg: GenMessage) {
-        console.log("Sending message!", this.ws);
-        let frame: Frame = {
-            type: 'GenMessage',
-            data: {
-                body: msg.body,
-            },
-        };
-        console.log("sending frame");
-        this.ws.next(JSON.stringify(frame));
+  private socket: Subject<MessageEvent>;
+  url: string = DRIVER_URL;
+
+  public connect(): Subject<MessageEvent> {
+    if(!this.socket) {
+      this.socket = this.create(this.url);
     }
+    return this.socket;
+  }
 
-}
-
-function parseFrame(frame: Frame): Message {
-    console.log("Parsing frame.");
-    if (frame.type === 'GenMessage') {
-        let msg = new GenMessage();
-        msg.body = frame.data.body;
-        return msg;
-    } else {
-        console.log(frame);
-    }
-    return null;
-}
-
-function makeHot<T>(cold: Observable<T>): Observable<T> {
-    console.log("Making it hot.");
-    let subject = new Subject();
-    let refs = 0;
-    return Observable.create((observer: Observer<T>) => {
-        let coldSub: Subscription;
-        if (refs === 0) {
-            coldSub = cold.subscribe(o => subject.next(o));
+  private create(url: string): Subject<MessageEvent> {
+    let ws = new WebSocket(url);
+    let observable = Observable.create(
+        (obs: Observer<MessageEvent>) => {
+            ws.onmessage = obs.next.bind(obs);
+            ws.onerror = obs.error.bind(obs);
+            ws.onclose = obs.complete.bind(obs);
+            return ws.close.bind(ws);
         }
-        refs++;
-        let hotSub = subject.subscribe(observer);
-        return () => {
-            refs--;
-            if (refs === 0) {
-                coldSub.unsubscribe();
+    );
+    let observer = {
+        next: (data: Object) => {
+            if (ws.readyState === WebSocket.OPEN) {
+                ws.send(JSON.stringify(data));
             }
-            hotSub.unsubscribe();
-        };
-    });
+        },
+    };
+    return Subject.create(observer, observable);
+  }
+
 }
