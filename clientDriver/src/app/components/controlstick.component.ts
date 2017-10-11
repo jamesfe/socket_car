f728abaa18477b8d21dbde559a3219b05a6c5ba9
jamesfe <james.ferrara@gmail.com>
2017-10-11 22:12:02 +0200

    sendig messages to the server

--- clientDriver/src/app/components/controlstick.component.ts
+++ clientDriver/src/app/components/controlstick.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SocketService } from '../services/serversocket.service';
+import { SocketService, Message } from '../services/serversocket.service';
 
 @Component({
   selector: 'controlstick',
@@ -8,17 +8,36 @@ import { SocketService } from '../services/serversocket.service';
 })
 
 export class ControlStickComponent {
-
-  private message = {
-		purpose : 'turn',
-		message: ''
-	};
+  private turnval = 5; // how fast do we turn?
+  private speedinc = 5; // how fast do we turn?
 
   constructor(private socketService: SocketService) { }
 
   clickAction(action: string) {
-    console.log("Sending...");
-    var a = this.socketService.messages.next(this.message);
-    console.log(action);
+    var message: Message;
+    switch (action) {
+      case "left":
+        message = {action: "turn", direction: "left", value: this.turnval}
+        break;
+      case "right":
+        message = {action: "turn", direction: "right", value: this.turnval}
+        break;
+      case "incspeed":
+        message = {action: "speed", left: true, right: true, value: this.speedinc}
+        break;
+      case "decspeed":
+        message = {action: "speed", left: true, right: true, value: -1 * this.speedinc}
+        break;
+      case "zero":
+        message = {action: "zero"}
+        break;
+      case "stop":
+        message = {action: "stop"}
+        break;
+      default:
+        console.log("Did not receive a valid action.");
+    }
+    console.log("Sending..." + message);
+    var a = this.socketService.messages.next(message);
   }
 }
