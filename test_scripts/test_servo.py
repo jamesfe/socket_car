99cb59cc01688c7f10e30342cd1d2f261126f1a0
jamesfe <james.ferrara@gmail.com>
2018-01-02 21:00:55 +0000

    testscript touchups

--- test_scripts/test_servo.py
+++ test_scripts/test_servo.py
@@ -1,26 +1,27 @@
 # Servo Control
 import time
-import wiringpi
+import wiringpi as wi
 
+print('setup')
 # use 'GPIO naming'
-wiringpi.wiringPiSetupGpio()
-
+wi.wiringPiSetupGpio()
+num = 1
 # set #18 to be a PWM output
-wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
-
+wi.pinMode(num, wi.GPIO.PWM_OUTPUT)
+import pdb; pdb.set_trace()
+print('pin')
 # set the PWM mode to milliseconds stype
-wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+wi.pwmSetMode(wi.GPIO.PWM_MODE_MS)
 
+print('clock')
 # divide down clock
-wiringpi.pwmSetClock(192)
-wiringpi.pwmSetRange(2000)
+wi.pwmSetClock(192)
+wi.pwmSetRange(2000)
 
-delay_period = 0.01
 
-while True:
-    for pulse in range(50, 250, 1):
-        wiringpi.pwmWrite(18, pulse)
-        time.sleep(delay_period)
-    for pulse in range(250, 50, -1):
-        wiringpi.pwmWrite(18, pulse)
-        time.sleep(delay_period)
+print('pulse')
+pulse = -200
+wi.pwmWrite(num, pulse)
+time.sleep(1)
+print('done')
+# wi.pwmWrite(num, pulse-200)
