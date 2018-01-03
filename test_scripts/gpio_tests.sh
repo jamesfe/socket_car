bcb612c732ae0ce54d3378ce48c2099cba26b04b
jamesfe <james.ferrara@gmail.com>
2018-01-03 19:35:34 +0000

    more test scripts

--- test_scripts/gpio_tests.sh
+++ test_scripts/gpio_tests.sh
@@ -3,5 +3,19 @@ gpio pwmc 192
 gpio pwmr 2000
 gpio mode 1 pwm
 gpio pwm 1 2000
+gpio pwm 1 2000
+gpio pwm 1 2000
+gpio pwm 1 2000
+gpio pwm 1 2000
+gpio pwm 1 2000
+gpio pwm 1 2000
 sleep 1s
 gpio pwm 1 100
+gpio pwm 1 100
+gpio pwm 1 100
+gpio pwm 1 100
+gpio pwm 1 100
+gpio pwm 1 100
+gpio pwm 1 100
+gpio pwm 1 100
+sleep 1s
