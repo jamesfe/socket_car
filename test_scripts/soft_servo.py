b4755f52d143af648a59fe654134bdc288d8a783
jamesfe <james.ferrara@gmail.com>
2018-01-11 20:51:48 +0000

    test ranges

--- test_scripts/soft_servo.py
+++ test_scripts/soft_servo.py
@@ -21,6 +21,21 @@ def setup_and_test(pin, small, large, first):
     print('done sleeping')
     wiringpi.softPwmWrite(servo_gpio_pin, small)
 
-for l in range(0, 1000, 50):
-    setup_and_test(servo_gpio_pin, 0, l, int(l/2))
+# for l in range(0, 1000, 50):
+#     setup_and_test(servo_gpio_pin, 0, l, int(l/2))
 
+
+print('setup results: {}'.format(wiringpi.wiringPiSetupGpio()))
+
+
+def testrange(top):
+    for i in range(0, top, int(top/10)):
+        print('sending {}'.format(i))
+        wiringpi.softPwmWrite(17, i)
+        time.sleep(.3)
+
+
+for k in range(10, 1000, 50):
+    print('creating pin', k)
+    wiringpi.softPwmCreate(17, 0, k)
+    testrange(k)
