8a3605b03df53df623acbd63492fd9c966be764b
jamesfe <james.ferrara@gmail.com>
2018-08-01 13:29:29 +0200

    Enable motor direction

--- test_scripts/combined_test.py
+++ test_scripts/combined_test.py
@@ -6,6 +6,7 @@ import wiringpi as wi
 
 
 def scale_to_motor(servo_min, servo_max, motor_min, motor_max):
+    # Currying: return a function that given our motor+servo ranges, will always scale to a valid value for the motor
     def rval(val):
         val = val - servo_min
         norm_max = servo_max - servo_min
@@ -30,6 +31,7 @@ args = parser.parse_args()
 servo_min = args.servo_min
 servo_max = args.servo_max
 servo_step = args.servo_step
+dir_pin = args.dir_pin
 
 print('setup')
 # use 'GPIO naming'
@@ -50,14 +52,19 @@ wi.pwmSetRange(2000)
 scale = scale_to_motor(servo_min, servo_max, args.motor_min, args.motor_max)
 
 for i in range(0, args.cycles):
+
+    # Indicate the motor should flow forward
+    wi.digitalWrite(dir_pin, 1)
     for x in range(servo_min, servo_max, servo_step):
-        print("To X: ", x)
+        print('To X: ', x)
         wi.pwmWrite(args.servo_pin, x)
         wi.pwmWrite(args.motor_pin, scale(x))
         time.sleep(.1)
 
+    # Indicate the motor should flow backward
+    wi.digitalWrite(dir_pin, 0)
     for x in range(servo_max, servo_min, servo_step):
-        print("To X: ", x)
+        print('To X: ', x)
         wi.pwmWrite(args.servo_pin, x)
         wi.pwmWrite(args.motor_pin, scale(x))
         time.sleep(.1)
